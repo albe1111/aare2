41ea395ae04f570c5d3074c8ed18a8ba868204cc
bberia <bberia@bbcag.ch>
2020-11-06 10:36:54 +0100

    added dist

---
+++ precache-manifest.62ac840413f9eb06a683c22946ce9f2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b41809e2c26288a3d9c",
+    "url": "/tets/aare/dist/css/app.a0c899db.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/tets/aare/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f7f97e031472c2b5354d1e8250f8bf11",
+    "url": "/tets/aare/dist/index.html"
+  },
+  {
+    "revision": "4b41809e2c26288a3d9c",
+    "url": "/tets/aare/dist/js/app.f8b9d2e6.js"
+  },
+  {
+    "revision": "0e095169c9f08821b2c3",
+    "url": "/tets/aare/dist/js/chunk-vendors.26f5c3c1.js"
+  },
+  {
+    "revision": "12c276d8bfc3171989fd9c83d2592236",
+    "url": "/tets/aare/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tets/aare/dist/robots.txt"
+  }
+]);
