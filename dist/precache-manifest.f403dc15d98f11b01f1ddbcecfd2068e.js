a13e04c3fcd019757f7bd188fdc9ea802a9030a8
bberia <bberia@bbcag.ch>
2020-11-06 10:45:21 +0100

    test

---
+++ dist/precache-manifest.f403dc15d98f11b01f1ddbcecfd2068e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68608611534d75d43c2b",
+    "url": "css/app.0300418b.css"
+  },
+  {
+    "revision": "3a3f2eb98e1a3a3864c67500b3e41cce",
+    "url": "index.html"
+  },
+  {
+    "revision": "68608611534d75d43c2b",
+    "url": "js/app.9c0cf347.js"
+  },
+  {
+    "revision": "0e095169c9f08821b2c3",
+    "url": "js/chunk-vendors.26f5c3c1.js"
+  },
+  {
+    "revision": "12c276d8bfc3171989fd9c83d2592236",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
